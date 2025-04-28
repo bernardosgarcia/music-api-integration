cb0be74e025b3b4fb79c170db91be301a4a5cb97
Natasha Fonseca <natashafonseca524@gmail.com>
2025-04-28 10:34:53 -0300

    Fix - Song model imports

--- src/models/songModel.ts
+++ src/models/songModel.ts
@@ -1,4 +1,8 @@
 import { UUID } from "crypto";
+<<<<<<< HEAD
+=======
+
+>>>>>>> c3e919e (Fix - Song model imports)
 import { SongStatusEnum } from "../enums/songStatusEnum";
 
 export interface SongModel extends Document {
