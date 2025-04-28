1dc4e70028c8ffe31344594ff121c2470cfbc7d2
Natasha Fonseca <natashafonseca524@gmail.com>
2025-04-28 08:53:29 -0300

    Feat - Added status field to song model

--- src/models/songModel.ts
+++ src/models/songModel.ts
@@ -1,9 +1,11 @@
 import { UUID } from "crypto";
+import { SongStatusEnum } from "../enums/SongStatusEnum";
 
 export interface SongModel extends Document{
     songId: UUID;
     songName: string;
     songVocalLink: string;
     songInstrumentalLink: string;
+    songStatus: SongStatusEnum;
     songCreatedAt: Date; 
 }
