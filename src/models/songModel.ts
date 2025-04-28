c3e919ed38bdb12be879c4ebcd42c05b9658b114
Natasha Fonseca <natashafonseca524@gmail.com>
2025-04-28 10:34:53 -0300

    Fix - Song model imports

--- src/models/songModel.ts
+++ src/models/songModel.ts
@@ -1,9 +1,6 @@
 import { UUID } from "crypto";
-<<<<<<< HEAD
+
 import { SongStatusEnum } from "../enums/songStatusEnum";
-=======
-import { SongStatusEnum } from "../enums/SongStatusEnum";
->>>>>>> 5e1fbc5e02d42cd583c12bcf83df6a3ceed89a39
 
 export interface SongModel extends Document{
     songId: UUID;
