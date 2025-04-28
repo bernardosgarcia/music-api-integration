d8d2d5bdd35ca0db81ebd5478a0c9136fd4f45e6
Natasha Fonseca <natashafonseca524@gmail.com>
2025-04-28 10:16:44 -0300

    Fix - Enum file name import

--- src/models/songModel.ts
+++ src/models/songModel.ts
@@ -1,5 +1,5 @@
 import { UUID } from "crypto";
-import { SongStatusEnum } from "../enums/SongStatusEnum";
+import { SongStatusEnum } from "../enums/songStatusEnum";
 
 export interface SongModel extends Document {
     songId: UUID;
