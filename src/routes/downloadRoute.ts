cf181bb509e6c81902a46359ddd2a52d2fe21f75
Murilo Kauan Socek <socekmurilo@gmail.com>
2025-05-21 19:52:38 -0300

    add documentation in download routes :memo:

--- src/routes/downloadRoute.ts
+++ src/routes/downloadRoute.ts
@@ -3,7 +3,60 @@ import { downloadController } from '../controllers/downloadController';
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/download/song:
+ *   post:
+ *     summary: Realizar download da Música
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               song:
+ *                 type: string
+ *               author:
+ *                 type: string
+ *             required:
+ *               - song
+ *               - author
+ *     responses:
+ *       200:
+ *         description: Success
+ *       400:
+ *         description: Missing Download Song
+ */
 router.post("/song", downloadController.downloadSong)
+
+/**
+ * @swagger
+ * /api/download/playlist:
+ *   post:
+ *     summary: Realizar download da Playlist
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items:
+ *               type: object
+ *               properties:
+ *                 song:
+ *                   type: string
+ *                 author:
+ *                   type: string
+ *               required:
+ *                 - song
+ *                 - author
+ *     responses:
+ *       200:
+ *         description: Success
+ *       400:
+ *         description: Missing Download Playlist
+ */
 router.post("/playlist", downloadController.downloadPlaylist)
 
 export default router
